f7d3c055247760bb7157e74213ff5759216d7076
Mona Lisa <KEN.MCKELLER92@GMAIL.COM>
2023-08-10 11:52:01 -0500

    fixed index.js file in the models folder

--- models/index.js
+++ models/index.js
@@ -4,14 +4,31 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
-// Categories have many Products
-
-// Products belongToMany Tags (through ProductTag)
-
 // Tags belongToMany Products (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: "product_id"
+  },
+}),
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+    foreignKey: "product_id"
+  },
+}),
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+}),
 module.exports = {
   Product,
   Category,
